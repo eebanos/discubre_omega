12070ec2dd05c8b6d484dd1c4223990f371ce34a
Eddy Ernesto Baños Fernández <eebanos@gmail.com>
2014-10-15 14:49:22 +0200

    Alter user login form

--- template.php
+++ template.php
@@ -10,3 +10,33 @@
  * for your subtheme grows. Please read the README.txt in the /preprocess and /process subfolders
  * for more information on this topic.
  */
+
+function discubre_omega_theme(){
+    $items = array();
+    $items['user_login'] = array(
+        'render element' => 'form',
+        'path' => drupal_get_path('theme','discubre_omega').'/templates',
+        'template' => 'user-login',
+    );
+    return $items;
+}
+
+function discubre_omega_form_alter(&$form, &$form_state, $form_id){
+    //TODO Custom theme for login form
+    if($form_id == 'user_login'){
+        $form['name']['#prefix'] = '<div id="discubre-user" class="grid-4 prefix-1 suffix-1 alpha omega">';
+        $form['name']['#attributes'] = array(
+            'placeholder' => t('Nombre de usuario'),
+        );
+        $form['name']['#title'] = NULL;
+        $form['name']['#suffix'] = '</div>';
+
+        $form['pass']['#prefix'] = '<div id="discubre-pass" class="grid-4 prefix-1 suffix-1 alpha omega">';
+        $form['pass']['#title'] = NULL;
+        $form['pass']['#suffix'] = '</div>';
+
+        $form['actions']['submit']['#prefix'] = '<div id="discubre-submit" class="grid-4 prefix-1 suffix-1 alpha omega">';;
+        $form['actions']['submit']['#suffix'] = '</div>';
+        $form['actions']['submit']['#id'] = 'login-submit';
+    }
+}
---
+++ templates/user-login.tpl.php
@@ -0,0 +1,7 @@
+<?php dpm($form);?>
+<div class="grid-6 alpha ">
+<?php print drupal_render_children($form);?>
+</div>
+<div class="grid-4 prefix-1">
+    <h2 class="salesMsg"><?php print t('¿Aún no eres cliente?');?></h2>
+</div>
